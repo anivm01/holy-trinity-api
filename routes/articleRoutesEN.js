cf49faf248b1601a1e0ecaec3f6656f3007a913c
Ani Mihaylova <anivm.webdev@gmail.com>
2023-01-31 17:44:19 -0500

    add forgotten article routes

---
+++ routes/articleRoutesEN.js
@@ -0,0 +1,17 @@
+const router = require ("express").Router()
+const articleController = require("../controllers/articleControllerEN")
+
+router
+.route("/")
+.get(articleController.readAll)
+.post(articleController.create);
+
+
+router
+.route("/:id")
+.get(articleController.readSingle)
+.put(articleController.updateSingle)
+.delete(articleController.deleteSingle);
+
+
+module.exports = router;
